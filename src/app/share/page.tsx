18b5e32de213cf1c4a175c73e23b2d7e6a599f80
availe <94802525+availe@users.noreply.github.com>
2024-11-28 00:46:23 -0500

    qr dynamic size

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
-import React from 'react';
-import { QRCodeCanvas } from 'qrcode.react';
+import React, { useState, useEffect } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 
 const SharePage: React.FC = () => {
-  const url = "https://spy-arbitrage-dashboard-xe57t1ng9-availes-projects.vercel.app";
+  const url =
+    "https://spy-arbitrage-dashboard-xe57t1ng9-availes-projects.vercel.app";
+  const [qrSize, setQrSize] = useState(window.innerWidth < 640 ? 200 : 400);
+
+  useEffect(() => {
+    const updateQrSize = () => {
+      setQrSize(window.innerWidth < 640 ? 200 : 400);
+    };
+
+    window.addEventListener("resize", updateQrSize);
+
+    updateQrSize();
+
+    return () => {
+      window.removeEventListener("resize", updateQrSize);
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col h-[85vh] items-center justify-center gap-4'>
-      <QRCodeCanvas value={url} size={400} />
-      <p className='text-center'>{url}</p>
+    <div className="flex flex-col h-[85vh] items-center justify-center gap-4">
+      <QRCodeCanvas value={url} size={qrSize} />
+      <p className="text-center px-2">{url}</p>
     </div>
   );
 };
