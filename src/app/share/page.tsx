f46ac844e6be7412bd621d7b9235d70d63e108ee
availe <94802525+availe@users.noreply.github.com>
2024-11-28 00:41:38 -0500

    change anchors to Link

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 
 const SharePage: React.FC = () => {
-  const url = "https://availe.io";
+  const url = "https://spy-arbitrage-dashboard-xe57t1ng9-availes-projects.vercel.app";
   return (
     <div className='flex flex-col h-[85vh] items-center justify-center gap-4'>
       <QRCodeCanvas value={url} size={400} />
