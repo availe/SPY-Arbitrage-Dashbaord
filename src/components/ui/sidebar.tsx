15710d23f20f670ad34150534f30a717c139722d
availe <94802525+availe@users.noreply.github.com>
2024-11-28 20:25:53 -0500

    increased size of nav trigger icon

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
-import { Menu } from "lucide-react";
+import { PanelLeftOpenIcon } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
@@ -285,14 +285,14 @@ const SidebarTrigger = React.forwardRef<
       ref={ref}
       data-sidebar="trigger"
       variant="ghost"
-      size="lg"
+      className="p-6"
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
       }}
       {...props}
     >
-      <Menu />
+      <PanelLeftOpenIcon style={{ width: '24px !important', height: '24px !important' }} />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
